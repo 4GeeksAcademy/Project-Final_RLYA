773db54f8af33f17c5fadd10801fffaa5bb8347b
Yoselin Calvetti <138071248+YoselinCalvetti@users.noreply.github.com>
2023-09-11 15:58:58 +0000

    commit

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,8 +7,25 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			login
+		<div className="d-flex align-items-center justify-content-center vh-100">
+		<div className="container p-5 col-4 border rounded-3 shadow">
+			<form>
+  <div className="mb-3">
+	<h3 className="text-center mb-5"><strong>Iniciar Sesión</strong></h3>
+    <label for="exampleInputEmail1" className="form-label text-start fs-6"><strong>Email</strong></label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="form-text"></div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label text-start fs-6"><strong>Password</strong></label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+	<div id="emailHelp" className="form-text text-center">¿No tienes una cuenta?<strong className="text-success">Crear una cuenta</strong></div>
+	<div className="text-center">
+  <button type="submit" className="btn btn-dark mt-3">Iniciar sesión</button>
+  </div>
+</form>
+		</div>
 		</div>
 	);
 };
