c353a3171790c95cbfdbf75e50195002ba8d41b0
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-09-20 14:27:44 +0000

    commit

--- src/front/js/component/Sidebar.js
+++ src/front/js/component/Sidebar.js
@@ -13,20 +13,17 @@ export const Sidebar = () => {
 				</div>
 				<div className="menu" >
 					{/*Momentaneo*/}
-					{
-						store.user.rol == "user" ? <div className="d-flex flex-column">
-							<a className="d-block p-3 text-decoration-none text-light" href="/listprof"><i class="fa-solid fa-earth-americas"></i> Servicios</a>
-							<Link to="/agendas_activas" className="d-block p-3 text-decoration-none text-light">
-								<i className="far fa-calendar lead text-light"></i> Agendas activas
-							</Link>
-							<a className="d-block p-3 text-decoration-none text-light" onClick={() => actions.Logout()} href="#"><i class="fa-solid fa-arrow-right-from-bracket lead text-light"></i>  Logout</a>
 
-						</div> : <div className="d-flex flex-column">
-							<a className="d-block p-3 text-decoration-none text-light" href={"/calendario_empresa" + store.user.id}><i className="fa-solid fa-envelope mx-2 mx-2 lead text-light"></i> Configuración</a>
-							<a className="d-block p-3 text-decoration-none text-light" href="#"><i className="fa-solid fa-gear mx-2 mx-2 lead text-light"></i> Configuración</a>
-							<a className="d-block p-3 text-decoration-none text-light" onClick={() => actions.Logout()} href="#"><i class="fa-solid fa-arrow-right-from-bracket mx-2 lead text-light"></i>  Logout</a>
-						</div>
-					}
+					<div className="d-flex flex-column">
+						<Link to="/listprof" className="d-block p-3 text-decoration-none text-light">
+						<i class="fa-solid fa-earth-americas"></i> Servicios
+						</Link>
+						<Link to="/agendas_activas" className="d-block p-3 text-decoration-none text-light">
+							<i className="far fa-calendar lead text-light"></i> Agendas activas
+						</Link>
+						<a className="d-block p-3 text-decoration-none text-light" onClick={() => actions.Logout()} href="#"><i class="fa-solid fa-arrow-right-from-bracket lead text-light"></i>  Logout</a>
+					</div>
+
 				</div>
 			</div>
 		</div>
